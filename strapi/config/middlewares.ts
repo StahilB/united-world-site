41308178867d0e87f2fb8ce28bf368327354f7aa
StahilB <bukinstahil@gmail.com>
2026-04-10 21:54:20 +0300

    Modern text redactor Strapi

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -3,7 +3,20 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.ckeditor.com'],
+          'style-src': ["'self'", "'unsafe-inline'", 'cdn.ckeditor.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'cdn.ckeditor.com', 'market-assets.strapi.io'],
+          'connect-src': ["'self'", 'https:'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
